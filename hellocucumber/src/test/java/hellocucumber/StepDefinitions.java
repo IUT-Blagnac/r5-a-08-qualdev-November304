1688bf7b249208d7304c34cee7fa5f0c3720da37
Nathan Pagnucco <105515410+November304@users.noreply.github.com>
2025-01-06 13:31:36 +0100

    Première version

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -20,18 +20,26 @@ public class StepDefinitions {
 
     @Given("today is Sunday")
     public void today_is_sunday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        IsItFriday.day = "Sunday";
     }
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_friday_yet() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        IsItFriday.isDayFriday = IsItFriday.isItFriday(IsItFriday.day);
     }
     @Then("I should be told {string}")
     public void i_should_be_told(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert IsItFriday.isDayFriday.equals(string);
     }
 
+}
+
+class IsItFriday {
+
+    static String day;
+
+    static String isItFriday(String today) {
+        return "Nope";
+    }
+
+    static String isDayFriday;
 }
